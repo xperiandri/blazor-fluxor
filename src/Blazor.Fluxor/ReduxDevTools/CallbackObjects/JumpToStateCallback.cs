db31b6a539429b1d00becdcbbb4a59f142146649
Peter Morris <mrpmorris@gmail.com>
2018-06-13 18:52:47 +0100

    Remove duplicate property

--- src/Blazor.Fluxor/ReduxDevTools/CallbackObjects/JumpToStateCallback.cs
+++ src/Blazor.Fluxor/ReduxDevTools/CallbackObjects/JumpToStateCallback.cs
@@ -5,7 +5,6 @@ namespace Blazor.Fluxor.ReduxDevTools.CallbackObjects
 	internal class JumpToStateCallback: BaseCallbackObject<JumpToStatePayload>
 	{
 #pragma warning disable IDE1006 // Naming Styles
-		public string type { get; set; }
 		public string state { get; set; }
 #pragma warning restore IDE1006 // Naming Styles
 	}
