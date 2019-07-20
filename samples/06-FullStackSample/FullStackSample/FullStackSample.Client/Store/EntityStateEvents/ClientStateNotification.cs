2c0ae64ec8a12ee861e192e18c04f7b5a6df74f1
Peter Morris <mrpmorris@gmail.com>
2019-07-20 20:36:33 +0100

    Entity state events

---
+++ samples/06-FullStackSample/FullStackSample/FullStackSample.Client/Store/EntityStateEvents/ClientStateNotification.cs
@@ -0,0 +1,17 @@
+﻿
+using Blazor.Fluxor;
+
+namespace FullStackSample.Client.Store.EntityStateEvents
+{
+	public class ClientStateNotification : IAction
+	{
+		public int Id { get; set; }
+		public PropertyUpdate<string> Name { get; set; }
+
+		public ClientStateNotification(int id, PropertyUpdate<string> name)
+		{
+			Id = id;
+			Name = name;
+		}
+	}
+}
