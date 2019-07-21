4b194d76c5b1600ae680f58703fe23efcdbb8099
Peter Morris <mrpmorris@gmail.com>
2019-07-21 17:42:32 +0100

    Breadcrumbs

---
+++ samples/06-FullStackSample/FullStackSample/FullStackSample.Api/Validators/ClientValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FullStackSample.Api.Models;
+
+namespace FullStackSample.Api.Validators
+{
+	public class ClientValidator : AbstractValidator<Client>
+	{
+		public ClientValidator()
+		{
+			RuleFor(x => x.Name)
+				.NotEmpty()
+				.WithMessage($"{nameof(Client.Name)} required");
+		}
+	}
+}
