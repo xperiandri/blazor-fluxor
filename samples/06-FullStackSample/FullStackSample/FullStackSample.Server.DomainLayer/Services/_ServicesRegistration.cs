16405ab8d1757f131268c1463feb478873369240
Peter Morris <mrpmorris@gmail.com>
2019-07-26 20:13:12 +0100

    Register fluent validation"

--- samples/06-FullStackSample/FullStackSample/FullStackSample.Server.DomainLayer/Services/_ServicesRegistration.cs
+++ samples/06-FullStackSample/FullStackSample/FullStackSample.Server.DomainLayer/Services/_ServicesRegistration.cs
@@ -3,9 +3,9 @@ using FullStackSample.Api.Requests;
 using FullStackSample.Server.DomainLayer.RequestHandlers;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
-using Microsoft.EntityFrameworkCore.Relational.Query.Pipeline;
 using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+using System.Reflection;
 
 namespace FullStackSample.Server.DomainLayer.Services
 {
@@ -16,6 +16,7 @@ namespace FullStackSample.Server.DomainLayer.Services
 			RegisterRequestHandlers(services);
 			RegisterRepositories(services);
 			RegisterServices(services);
+			RegisterValidators(services);
 
 			services.AddAutoMapper(typeof(ClientsSearchQueryHandler).Assembly);
 			services.AddMediatR(typeof(ClientsSearchQueryHandler).Assembly);
@@ -32,7 +33,7 @@ namespace FullStackSample.Server.DomainLayer.Services
 		private static void RegisterRequestHandlers(IServiceCollection services)
 		{
 			services.AddScoped<IRequestHandler<ClientsSearchQuery, ClientsSearchResponse>, ClientsSearchQueryHandler>();
-			services.AddScoped<IRequestHandler<ClientCreateCommand, ClientCreateResponse>>();
+			services.AddScoped<IRequestHandler<ClientCreateCommand, ClientCreateResponse>, ClientCreateCommandHandler>();
 		}
 
 		private static void RegisterRepositories(IServiceCollection services)
@@ -44,6 +45,16 @@ namespace FullStackSample.Server.DomainLayer.Services
 		private static void RegisterServices(IServiceCollection services)
 		{
 			services.AddScoped<IUnitOfWork, UnitOfWork>();
+		}
+
+		private static void RegisterValidators(IServiceCollection services)
+		{
+			var assemblies = new Assembly[]
+			{
+				typeof(ApiResponse).Assembly,
+				typeof(ServicesRegistration).Assembly
+			};
+			services.AddValidatorsFromAssemblies(assemblies, ServiceLifetime.Scoped);
 		}
 	}
 }
