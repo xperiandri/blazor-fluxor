3f193cf9148497b7ff505f6969c2da591754d9e0
Peter Morris <mrpmorris@gmail.com>
2019-08-31 16:03:44 +0100

    MultiActionReducer

---
+++ samples/06-FullStackSample/FullStackSample/FullStackSample.Client/Store/ClientCreate/ClientCreateReducers.cs
@@ -0,0 +1,41 @@
+﻿using Blazor.Fluxor;
+using Blazor.Fluxor.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullStackSample.Client.Store.ClientCreate
+{
+	public class ClientCreateReducers : MultiActionReducer<ClientCreateState>
+	{
+		public ClientCreateReducers()
+		{
+			AddActionReducer<Go>((state, action) =>
+			{
+				string uri = new Uri(action.NewUri ?? "").AbsolutePath.ToLowerInvariant();
+				if (uri.StartsWith("/clients/create"))
+					return state;
+				return new ClientCreateState(
+					client: null,
+					isExecutingApi: false,
+					errorMessage: null,
+					validationErrors: null);
+			});
+
+			AddActionReducer<Api.Requests.ClientCreateCommand>((state, action) =>
+				new ClientCreateState(
+								client: state.Client,
+								isExecutingApi: true,
+								errorMessage: null,
+								validationErrors: null));
+
+			AddActionReducer<Api.Requests.ClientCreateResponse>((state, action) =>
+				new ClientCreateState(
+					client: state.Client,
+					isExecutingApi: false,
+					errorMessage: action.ErrorMessage,
+					validationErrors: action.ValidationErrors));
+		}
+	}
+}
---
+++ src/Blazor.Fluxor/MultiActionReducer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blazor.Fluxor
+{
+	public abstract class MultiActionReducer<TState> : IReducer<TState>
+	{
+		Dictionary<Type, Func<TState, object, TState>> ReducersByActionType;
+
+		public MultiActionReducer()
+		{
+			ReducersByActionType = new Dictionary<Type, Func<TState, object, TState>>();
+		}
+
+		public void AddActionReducer<TAction>(Func<TState, TAction, TState> reducer)
+			=> AddActionReducer(typeof(TAction), reducer);
+
+		public void AddActionReducer(Type actionType, Func<TState, object, TState> reducer)
+			=> ReducersByActionType.Add(actionType, reducer);
+
+		public TState Reduce(TState state, object action)
+		{
+			if (action == null)
+				return state;
+
+			if (!ReducersByActionType.TryGetValue(action.GetType(), out Func<TState, object, TState> reducer))
+				throw new InvalidOperationException($"Reducer {GetType().Name} cannot reduce action {action.GetType().Name} into state {typeof(TState).Name}.");
+
+			return reducer(state, action);
+		}
+
+		public bool ShouldReduceStateForAction(object action)
+		{
+			if (action == null)
+				return false;
+
+			return ReducersByActionType.ContainsKey(action.GetType());
+		}
+	}
+}
